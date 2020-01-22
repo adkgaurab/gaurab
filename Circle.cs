1a3c5e7287164846fe2f816a87353d2a34ffa459
adkgaurab <59887105+adkgaurab@users.noreply.github.com>
2020-01-22 21:24:18 +0545

    Add files via upload

---
+++ Circle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASE_Component_I
+{
+    /// <summary>
+    /// class to draw circle
+    /// </summary>
+    class Circle: Shape
+    {
+        private int r, x, y;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a">origin x- coordinate</param>
+        /// <param name="b">origin y- coordinate</param>
+        /// <param name="c">radius of circle</param>
+        public void saved_values(int a, int b, int c) {
+            x = a;
+            y = b;
+            r = c;
+        }
+        public override void Draw_shape(Graphics g)
+        {
+            Pen mew2 = new Pen(Color.Black,2);
+            g.DrawEllipse(mew2, x, y, r, r);
+        }
+    }
+}
